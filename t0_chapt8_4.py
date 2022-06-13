31a9868e1509bcfe3ba0e3d9ccccf75f35d338db
S-Li <s-li@test.ml>
2022-06-13 15:54:50 +0300

    Chapt8: Exercise 14/14

--- t0_chapt8_4.py
+++ t0_chapt8_4.py
@@ -328,7 +328,20 @@ for thing in ('Got %s' % nmbr for nmbr in range(10)):
 
 print('\n\n\n', '#EXERCISE 8.13')
 # -----------------------------------------------
+keys = ('optimist', 'pessimist', 'troll')
+values = ('The glass is half full',
+          'The glass is half empty',
+          'How did you get a glass?')
+print(dict(zip(keys, values)))
 
 
 print('\n\n\n', '#EXERCISES 8.14')
 # -----------------------------------------------
+titles = ['Creature of Habit',
+          'Crewel Fate',
+          'Sharks On a Plane']
+plots = ['A nun turns into a monster',
+         'A haunted yarn shop',
+         'Check your exits']
+movies = dict(zip(titles, plots))
+print(movies)
